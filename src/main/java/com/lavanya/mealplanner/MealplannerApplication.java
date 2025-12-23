8fe1a1c93d8914d1fa3c0fcbf300077c99c1ec72
Lavanya Paleti <srilavanya.paleti@gmail.com>
2025-12-22 17:10:02 -0700

    feat: bootstrap Spring Boot project

---
+++ src/main/java/com/lavanya/mealplanner/MealplannerApplication.java
@@ -0,0 +1,13 @@
+package com.lavanya.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lavanya/mealplanner/MealplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavanya.mealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
